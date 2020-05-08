e31742e746345d7467de23329d090a5914d47b1b
Max Zueff <maxx.zueff@gmail.com>
2020-05-08 22:48:40 +0300

    express+next

---
+++ secret.js
@@ -0,0 +1,5 @@
+exports.vk = {
+    "clientID": "7369914",
+    "clientSecret": "bbHD0OWOef1IiSzr4squ",
+    "callbackURL": "http://localhost:8080/auth/vk/callback"
+}
--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const http = require('http');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const logger = require('morgan');
 const config = require('./config').port;
 const passport = require('passport');
+const cacheableResponse = require('cacheable-response');
+const next = require('next');
 
-// const bodyParser = require('body-parser');
+const dev = process.env.NODE_ENV !== 'production'
+const appNext = next({ dev });
+const handle = appNext.getRequestHandler();
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -18,15 +21,9 @@ const index = require('./routers/index');
 const user = require('./routers/user');
 
 // ------------------------------------------------------------------
-// Server
-
-const app = express();
-const server = http.createServer(app);
+// DataBase
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-
-// ------------------------------------------------------------------
-// Middleware function handles
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -34,85 +31,84 @@ db.once('open', function() {
   console.log('we\'re connected!');
 });
 
-require('./models/Users');
-
-app.use(express.json());
-// app.use(bodyParser);
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore({mongooseConnection: db})}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// function error_handler (err, req, res, next) {
-
-//     if (config.errors[err.code]) {
-//         if (err.body) err.body.message = config.errors[err.code];
-//         else err.body = {message: config.errors[err.code]};
-//     }
-
-//     console.log(err);
-
-//     res.status(err.status || 500);
-//     res.json(err.body);
-// }
-
 // ------------------------------------------------------------------
-// Template engine
+// Cache
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const ssrCache = cacheableResponse({
+  ttl: 1000 * 60 * 60, // 1hour
+  get: async ({ req, res, pagePath, queryParams }) => {
+    const data = await app.renderToHTML(req, res, pagePath, queryParams)
 
-// ------------------------------------------------------------------
-// Middleware for every path
+    // Add here custom logic for when you do not want to cache the page, for
+    // example when the page returns a 404 status code:
+    if (res.statusCode === 404) {
+      res.end(data)
+      return
+    }
 
-app.use(logger('dev'));
-app.use(express.static('./public'));
-
-app.use(function(req, res, next) {
-  if (req.session) {
-    console.log(req.session);
-    next();
-  } else {
-    console.log('no session in req');
-    next();
-  }
+    return { data }
+  },
+  send: ({ data, res }) => res.send(data),
 });
 
-app.use(user);
-app.use(index);
 
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
+appNext.prepare().then(() => {
+
+  const app = express();
+  const server = http.createServer(app);
+
+  app.use(express.json());
+
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({mongooseConnection: db})}));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(logger('dev'));
+
+  app.use(function(req, res, next) {
+    if (req.session) {
+      console.log(req.session);
+      next();
+    } else {
+      console.log('no session in req');
+      next();
+    }
+  });
+
+  app.use(user);
+  app.use(index);
+
+  app.post('/login', (req, res) => {
+    console.log(req.body);
+    sess = req.session;
+    sess.email = req.body.email;
+    res.end('done');
+  });
+
+  app.get('*', (req, res) => handle(req, res));
+
+  // ------------------------------------------------------------------
+  // Error handler for paths
+
+  app.use(function notFound(req, res, next) {
+    next({
+      status: 404,
+      code: 404});
+  });
+
+  // app.use(error_handler);
+
+  // ------------------------------------------------------------------
+  // Server listener
+
+  server.listen(config.port, function() {
+    console.log( 'http://localhost:' + config.port +
+      ' : server has been launched' );
+  });
+
 });
-
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  sess = req.session;
-  sess.email = req.body.email;
-  res.end('done');
-});
-
-// ------------------------------------------------------------------
-// Error handler for paths
-
-app.use(function notFound(req, res, next) {
-  next({
-    status: 404,
-    code: 404});
-});
-
-// app.use(error_handler);
-
-// ------------------------------------------------------------------
-// Server listener
-
-server.listen(config.port, function() {
-  console.log( 'http://localhost:' + config.port +
-    ' : server has been launched' );
-});
-
