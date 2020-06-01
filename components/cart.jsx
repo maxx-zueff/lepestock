45388106c0a13d5aff7a255a8d2f818ce2592533
Max Zueff <maxx.zueff@gmail.com>
2020-06-01 18:53:57 +0300

    cart

--- components/cart.jsx
+++ components/cart.jsx
@@ -1,4 +1,9 @@
 import styles from './cart.module.scss'
+import Trash from '../public/delete.svg'
+import Plus from '../public/more.svg'
+import Minus from '../public/minus.svg'
+import classNames from 'classnames'
+
 // import useClick from '../hooks/useClick'
 // import { useState } from "react";
 
@@ -10,7 +15,7 @@ export default function Component(props) {
 	}
 
 	let items = 1;
-	let subtotal = 6900;
+	let subtotal = 1590;
 
 	return (
 		<div className={props.toggle ? styles.open : null}>
@@ -31,8 +36,68 @@ export default function Component(props) {
 											</h1>
 											<div className={styles.cartSubtotal}>
 												Сумма заказа
-												<span className={styles.subtotal}>{subtotal} руб.</span>
+												<span className={styles.subtotal}>{subtotal} ₽</span>
 											</div>
+										</div>
+										<div className={styles.productContainer}>
+											<div className={styles.productWrapper}>
+												{/* <div className={styles.productHeader}>
+													<h2>Товары</h2>
+												</div> */}
+												<div className={styles.productList}>
+													<div className={styles.product}>
+														<a href="/">
+															<div className={styles.productImage}>
+																<img src="/products/1.jpg" alt=""/>
+															</div>
+														</a>
+														<div className={styles.productDescription}>
+															
+															<div className={styles.productCol}>
+																<span>Букетикcc для твоей любимой красотки</span>
+																<div className={styles.amountContainer}>
+																	<span className={styles.amountDescription}>Количество:</span>
+																	<div className={styles.amountChange}>
+																		<Minus />
+																	</div>
+																	<div className={styles.amountProduct}>1</div>																
+																	<div className={styles.amountChange}>
+																		<Plus />
+																	</div>
+																</div>
+															</div>
+
+															<div className={classNames(styles.productCol, styles.productCol_left)}>
+																<div className={styles.productPrice}>1590 ₽</div>
+																<div className={styles.btnIcon}>
+																	<Trash />
+																</div>
+															</div>
+														</div>
+													</div>
+													<div className={styles.productSection}>
+														<p className={styles.productDescription}>Выберите способ получения:</p>
+														<div className={styles.productReception}>
+															<div className={styles.itemReception}>
+																<div className={styles.productImage}>
+																	<img src="/delivery.png" alt=""/>
+																</div>
+																<p>Доставка</p>
+															</div>
+															<div className={styles.itemReception}>
+																<div className={styles.productImage}>
+																	<img src="/shop.png" alt=""/>
+																</div>
+																<p>В салоне</p>
+															</div>
+														</div>
+
+													</div>
+												</div>
+											</div>
+										</div>
+										<div>
+											<button className={styles.confirm}>Подтвердить заказ</button>
 										</div>
 									</div>
 								</div>
